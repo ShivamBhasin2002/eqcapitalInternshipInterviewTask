03c3287bbdf922e4c0be7e3eacce3f229cff1e4b
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-02-15 14:14:00 +0530

    added all required authentication process with login register and secret system in place

--- models/user.js
+++ models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, 'Username cannot be blank'],
-        unique: [true, 'Username should be unique']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password cannot be blank']
-    }
+	username: {
+		type: String,
+		required: [true, 'Username cannot be blank'],
+		unique: [true, 'Username should be unique']
+	},
+	password: {
+		type: String,
+		required: [true, 'Password cannot be blank']
+	},
+	courses: {
+		type: String
+	}
 });
 
 module.exports = mongoose.model('User', userSchema);
--- public/index.js
+++ public/index.js
@@ -1,26 +1,29 @@
-window.addEventListener("pageshow", function (event) {
-    let historyTraversal = event.persisted || (typeof window.performance != "undefined" && window.performance.navigation.type === 2);
-    if (historyTraversal) {
-        window.location.reload();
-    }
+window.addEventListener('pageshow', function (event) {
+	let historyTraversal =
+		event.persisted ||
+		(typeof window.performance != 'undefined' &&
+			window.performance.navigation.type === 2);
+	if (historyTraversal) {
+		window.location.reload();
+	}
 });
 
-document.getElementById("username").addEventListener("focusin", () => {
-    if (!document.getElementById("username").value)
-        document.getElementById("usernameLabel").classList.toggle("active");
+document.getElementById('username').addEventListener('focusin', () => {
+	if (!document.getElementById('username').value)
+		document.getElementById('usernameLabel').classList.toggle('active');
 });
 
-document.getElementById("username").addEventListener("focusout", () => {
-    if (!document.getElementById("username").value)
-        document.getElementById("usernameLabel").classList.toggle("active");
+document.getElementById('username').addEventListener('focusout', () => {
+	if (!document.getElementById('username').value)
+		document.getElementById('usernameLabel').classList.toggle('active');
 });
 
-document.getElementById("password").addEventListener("focusin", () => {
-    if (!document.getElementById("password").value)
-        document.getElementById("passwordLabel").classList.toggle("active");
+document.getElementById('password').addEventListener('focusin', () => {
+	if (!document.getElementById('password').value)
+		document.getElementById('passwordLabel').classList.toggle('active');
 });
 
-document.getElementById("password").addEventListener("focusout", () => {
-    if (!document.getElementById("password").value)
-        document.getElementById("passwordLabel").classList.toggle("active");
+document.getElementById('password').addEventListener('focusout', () => {
+	if (!document.getElementById('password').value)
+		document.getElementById('passwordLabel').classList.toggle('active');
 });
